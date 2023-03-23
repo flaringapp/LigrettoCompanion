263f40f75828a564882ac43d53e39ed58be8415c
Andrii Shpek <ashpek01@gmail.com>
2023-03-23 20:06:00 +0200

    Integrate splash screen

--- androidApp/src/main/java/com/flaringapp/ligretto/android/ui/main/MainActivity.kt
+++ androidApp/src/main/java/com/flaringapp/ligretto/android/ui/main/MainActivity.kt
@@ -3,11 +3,15 @@ package com.flaringapp.ligretto.android.ui.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.flaringapp.ligretto.android.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
+
         setContent {
             MyApplicationTheme {
                 MainScreen()
