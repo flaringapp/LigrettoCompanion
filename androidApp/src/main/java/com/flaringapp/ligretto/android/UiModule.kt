6be0aba01d58ee7877f2846d3fe6ea97a0cfd9f9
Andrii Shpek <ashpek01@gmail.com>
2023-03-27 12:07:08 +0300

    Add android ui di module definition

---
+++ androidApp/src/main/java/com/flaringapp/ligretto/android/UiModule.kt
@@ -0,0 +1,17 @@
+package com.flaringapp.ligretto.android
+
+import org.koin.core.KoinApplication
+import org.koin.core.annotation.ComponentScan
+import org.koin.core.annotation.Module
+import org.koin.core.annotation.Single
+import org.koin.ksp.generated.module
+
+internal fun KoinApplication.uiModule() = modules(UiModule().module)
+
+@Module
+@ComponentScan
+internal class UiModule {
+
+    @Single
+    fun stub(): String = "Hello"
+}
