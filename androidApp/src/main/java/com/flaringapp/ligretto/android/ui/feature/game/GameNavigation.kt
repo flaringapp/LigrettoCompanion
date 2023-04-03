aecb8a7b5e92a93550aa099757ad2055accdaa12
Andrii Shpek <ashpek01@gmail.com>
2023-04-03 11:16:39 +0300

    Close previous game screens when navigating to the next one

--- androidApp/src/main/java/com/flaringapp/ligretto/android/ui/feature/game/GameNavigation.kt
+++ androidApp/src/main/java/com/flaringapp/ligretto/android/ui/feature/game/GameNavigation.kt
@@ -2,6 +2,7 @@ package com.flaringapp.ligretto.android.ui.feature.game
 
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptionsBuilder
 import com.flaringapp.ligretto.android.ui.feature.game.end.GameEndDestination
 import com.flaringapp.ligretto.android.ui.feature.game.end.GameEndScreen
 import com.flaringapp.ligretto.android.ui.feature.game.lap.GameLapDestination
@@ -13,6 +14,7 @@ import com.flaringapp.ligretto.android.ui.feature.game.start.GameStartScreen
 import com.flaringapp.ligretto.android.ui.utils.navigation.ScreenDestinationWithoutArguments
 import com.flaringapp.ligretto.android.ui.utils.navigation.composable
 import com.flaringapp.ligretto.android.ui.utils.navigation.navigation
+import com.flaringapp.ligretto.android.ui.utils.navigation.popUpTo
 
 object GameDestination : ScreenDestinationWithoutArguments() {
 
@@ -48,17 +50,27 @@ fun NavGraphBuilder.gameGraph(navController: NavController) {
 }
 
 private fun NavController.navigateGameScores() {
-    navigate(GameScoreDestination.route())
+    navigate(GameScoreDestination.route()) {
+        closeGameScreens()
+    }
 }
 
 private fun NavController.navigateGameLap() {
-    navigate(GameLapDestination.route())
+    navigate(GameLapDestination.route()) {
+        closeGameScreens()
+    }
 }
 
 private fun NavController.navigateGameEnd() {
-    navigate(GameEndDestination.route())
+    navigate(GameEndDestination.route()) {
+        closeGameScreens()
+    }
 }
 
 private fun NavController.navigateCloseGame() {
     popBackStack(GameDestination.route(), true)
+}
+
+private fun NavOptionsBuilder.closeGameScreens() {
+    popUpTo(GameDestination)
 }
