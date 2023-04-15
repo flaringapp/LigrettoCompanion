5eef5a5186211c9965ad79d0d994c2323811c604
Andrii Shpek <ashpek01@gmail.com>
2023-04-15 23:30:45 +0300

    Add build logic module

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.flaringapp.ligretto.buildlogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+gradlePlugin {
+    plugins {
+    }
+}
