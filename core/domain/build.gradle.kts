75f5838dc3742b7ca8b56c730736601fa9f7bde0
Andrii Shpek <ashpek01@gmail.com>
2023-04-16 21:40:58 +0300

    Migrate kmm modules to use convention plugins

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -1,30 +1,9 @@
-// https://youtrack.jetbrains.com/issue/KTIJ-19369
-@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    id(libs.plugins.kotlin.multiplatform.get().pluginId)
-    id(libs.plugins.android.library.get().pluginId)
-    id(libs.plugins.ksp.get().pluginId)
+    id("ligretto.multiplatform.library")
+    id("ligretto.multiplatform.koin.ksp")
 }
 
 kotlin {
-    android {
-        compilations.all {
-            kotlinOptions {
-                jvmTarget = JavaVersion.VERSION_11.toString()
-            }
-        }
-    }
-
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64(),
-    ).forEach {
-        it.binaries.framework {
-            baseName = "core_domain"
-        }
-    }
-
     sourceSets {
         val commonMain by getting {
             dependencies {
@@ -37,46 +16,9 @@ kotlin {
                 api(libs.koin.annotations)
             }
         }
-        val commonTest by getting {
-            dependencies {
-                api(libs.kotlin.test)
-            }
-        }
-        val androidMain by getting
-        val androidUnitTest by getting
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-        val iosMain by creating {
-            dependsOn(commonMain)
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
-        }
-        val iosX64Test by getting
-        val iosArm64Test by getting
-        val iosSimulatorArm64Test by getting
-        val iosTest by creating {
-            dependsOn(commonTest)
-            iosX64Test.dependsOn(this)
-            iosArm64Test.dependsOn(this)
-            iosSimulatorArm64Test.dependsOn(this)
-        }
     }
 }
 
 android {
     namespace = "com.flaringapp.ligretto.domain"
-    compileSdk = 33
-    defaultConfig {
-        minSdk = 24
-    }
-}
-
-dependencies {
-    add("kspCommonMainMetadata", libs.koin.compiler)
-    add("kspAndroid", libs.koin.compiler)
-    add("kspIosX64", libs.koin.compiler)
-    add("kspIosArm64", libs.koin.compiler)
-    add("kspIosSimulatorArm64", libs.koin.compiler)
 }
