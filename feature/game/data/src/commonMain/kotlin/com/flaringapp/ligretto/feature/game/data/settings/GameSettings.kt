639ff2ca277d11c52446d2371c70e4a6612cbe1a
Andrii Shpek <ashpek01@gmail.com>
2023-08-21 15:29:55 +0300

    Add active game id flow to game settings

--- feature/game/data/src/commonMain/kotlin/com/flaringapp/ligretto/feature/game/data/settings/GameSettings.kt
+++ feature/game/data/src/commonMain/kotlin/com/flaringapp/ligretto/feature/game/data/settings/GameSettings.kt
@@ -2,15 +2,20 @@ package com.flaringapp.ligretto.feature.game.data.settings
 
 import com.flaringapp.ligretto.core.settings.provider.SettingsProvider
 import com.flaringapp.ligretto.core.settings.provider.SettingsType
+import com.russhwolf.settings.ExperimentalSettingsApi
 import com.russhwolf.settings.ObservableSettings
+import com.russhwolf.settings.coroutines.getLongOrNullFlow
 import com.russhwolf.settings.nullableLong
 import org.koin.core.annotation.Single
+import kotlinx.coroutines.flow.Flow
 
 internal interface GameSettings {
 
     var activeGameId: Long?
+    val activeGameIdFlow: Flow<Long?>
 }
 
+@OptIn(ExperimentalSettingsApi::class)
 @Single
 class GameSettingsImpl(
     private val settingsProvider: SettingsProvider,
@@ -21,4 +26,5 @@ class GameSettingsImpl(
     }
 
     override var activeGameId: Long? by settings.nullableLong("active_game_id")
+    override val activeGameIdFlow: Flow<Long?> = settings.getLongOrNullFlow("active_game_id")
 }
