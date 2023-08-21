1df7ebd5e4e2df31b1304edf6cffa5d23878d357
Andrii Shpek <ashpek01@gmail.com>
2023-08-21 15:33:25 +0300

    Fix test settings provider

--- core/settings-test/src/commonMain/kotlin/com/flaringapp/ligretto/core/settings/test/provider/TestSettingsProvider.kt
+++ core/settings-test/src/commonMain/kotlin/com/flaringapp/ligretto/core/settings/test/provider/TestSettingsProvider.kt
@@ -3,11 +3,11 @@ package com.flaringapp.ligretto.core.settings.test.provider
 import com.flaringapp.ligretto.core.settings.provider.SettingsProvider
 import com.flaringapp.ligretto.core.settings.provider.SettingsType
 import com.russhwolf.settings.MapSettings
-import com.russhwolf.settings.Settings
+import com.russhwolf.settings.ObservableSettings
 
 class TestSettingsProvider : SettingsProvider {
 
-    override fun provide(type: SettingsType): Settings {
+    override fun provide(type: SettingsType): ObservableSettings {
         return MapSettings()
     }
 }
