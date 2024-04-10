02d4bb905e095014fc13bc03df4cb3f1de198132
Andrii Shpek <ashpek01@gmail.com>
2024-04-10 22:57:44 +0300

    Remove obsolete android ksp sources applying

--- build-logic/convention/src/main/kotlin/AndroidKspConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidKspConventionPlugin.kt
@@ -1,9 +1,6 @@
-import com.android.build.api.variant.AndroidComponentsExtension
 import com.flaringapp.ligretto.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.plugins.ExtensionAware
-import org.jetbrains.kotlin.gradle.dsl.kotlinExtension
 
 class AndroidKspConventionPlugin : Plugin<Project> {
 
@@ -12,19 +9,5 @@ class AndroidKspConventionPlugin : Plugin<Project> {
             apply(libs.findPlugin("kotlin-android").get().get().pluginId)
             apply(libs.findPlugin("ksp").get().get().pluginId)
         }
-
-        androidComponents {
-            onVariants { variant ->
-                kotlinExtension.sourceSets.findByName(variant.name)?.kotlin?.srcDirs(
-                    file("$buildDir/generated/ksp/${variant.name}/kotlin")
-                )
-            }
-        }
     }
-}
-
-internal fun Project.androidComponents(
-    configure: AndroidComponentsExtension<*, *, *>.() -> Unit
-) {
-    (this as ExtensionAware).extensions.configure("androidComponents", configure)
 }
