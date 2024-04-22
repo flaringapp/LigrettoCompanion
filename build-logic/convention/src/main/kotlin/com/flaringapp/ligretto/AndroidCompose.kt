2b83e97f3df47edbde01a2b94de1ee4fb9380015
Andrii Shpek <ashpek01@gmail.com>
2024-04-22 15:04:02 +0300

    Remove android compose convention plugin config

--- build-logic/convention/src/main/kotlin/com/flaringapp/ligretto/AndroidCompose.kt
+++ build-logic/convention/src/main/kotlin/com/flaringapp/ligretto/AndroidCompose.kt
@@ -10,20 +10,7 @@ internal fun Project.configureAndroidCompose(
     commonExtension: CommonExtension<*, *, *, *, *>,
 ) {
     with(commonExtension) {
-        buildFeatures {
-            compose = true
-        }
-
-        composeOptions {
-            kotlinCompilerExtensionVersion =
-                libs.findVersion("composeCompiler").get().toString()
-        }
-
         dependencies {
-            val bom = libs.findLibrary("androidx-compose-bom").get()
-            add("implementation", platform(bom))
-            add("androidTestImplementation", platform(bom))
-
             add("lintChecks", libs.findLibrary("slack-lint-compose").get())
         }
     }
