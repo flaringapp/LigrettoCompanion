4f53803028dc0f401afaba167cb859be746547c9
Andrii Shpek <ashpek01@gmail.com>
2024-05-04 15:58:17 +0300

    Remove old core navigation module

--- core/navigation/src/main/java/com/flaringapp/ligretto/core/navigation/DialogDestination.kt
+++
@@ -1,8 +0,0 @@
-package com.flaringapp.ligretto.core.navigation
-
-import androidx.compose.ui.window.DialogProperties
-
-interface DialogDestination : ScreenDestination {
-
-    val dialogProperties: DialogProperties
-}
