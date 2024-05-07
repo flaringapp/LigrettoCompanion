8df87f02313c6ef281d627569a93ed9f662cae35
Andrii Shpek <ashpek01@gmail.com>
2024-05-07 14:13:01 +0300

    Add ios helper to run compose ui

---
+++ commonApp/src/appleMain/kotlin/com/flaringapp/ligretto/common/MainViewController.kt
@@ -0,0 +1,6 @@
+package com.flaringapp.ligretto.common
+
+import androidx.compose.ui.window.ComposeUIViewController
+import com.flaringapp.ligretto.common.ui.AppContent
+
+fun MainViewController() = ComposeUIViewController { AppContent() }
