3f9769f9ce01a92db14d0595fc5b10107a8c53d3
Andrii Shpek <ashpek01@gmail.com>
2024-05-07 14:28:44 +0300

    Add di and logging initialization on ios

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,12 +1,17 @@
 import SwiftUI
+import commonApp
 
 @main
 struct iOSApp: App {
+
+    init() {
+        Logging_appleKt.doInitLogging()
+        Di_appleKt.doInitDi()
+    }
+
 	var body: some Scene {
 		WindowGroup {
 			ContentView()
 		}
 	}
 }
-
-// TODO init napier
