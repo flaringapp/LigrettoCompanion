5a413edfb6d896b6d701870b0383fd9a2d639a59
Andrii Shpek <ashpek01@gmail.com>
2024-10-19 12:50:47 +0300

    Cleanup edge to edge setup

--- androidApp/src/main/java/com/flaringapp/ligretto/android/main/MainActivity.kt
+++ androidApp/src/main/java/com/flaringapp/ligretto/android/main/MainActivity.kt
@@ -5,43 +5,29 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.flaringapp.ligretto.common.ui.AppContent
+import android.graphics.Color as AndroidColor
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
 
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.auto(
+                AndroidColor.TRANSPARENT,
+                AndroidColor.TRANSPARENT,
+            ),
+            navigationBarStyle = SystemBarStyle.auto(
+                AndroidColor.TRANSPARENT,
+                AndroidColor.TRANSPARENT,
+            ),
+        )
+
         super.onCreate(savedInstanceState)
 
-        enableEdgeToEdge()
-
         setContent {
-            SystemBarsUi(this)
-
             AppContent()
         }
-    }
-}
-
-@Composable
-private fun SystemBarsUi(activity: ComponentActivity) {
-    val isDarkTheme = isSystemInDarkTheme()
-
-    DisposableEffect(isDarkTheme) {
-        activity.enableEdgeToEdge(
-            statusBarStyle = SystemBarStyle.auto(
-                android.graphics.Color.TRANSPARENT,
-                android.graphics.Color.TRANSPARENT,
-            ) { isDarkTheme },
-            navigationBarStyle = SystemBarStyle.auto(
-                android.graphics.Color.TRANSPARENT,
-                android.graphics.Color.TRANSPARENT,
-            ) { isDarkTheme },
-        )
-        onDispose {}
     }
 }
