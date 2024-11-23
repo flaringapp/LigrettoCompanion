7336a2ccd2f229ec787fea108f3874929c08a253
Andrii Shpek <ashpek01@gmail.com>
2024-11-23 13:18:39 +0200

    Fix ios on focus behavior

--- commonApp/src/appleMain/kotlin/com/flaringapp/ligretto/common/MainViewController.kt
+++ commonApp/src/appleMain/kotlin/com/flaringapp/ligretto/common/MainViewController.kt
@@ -1,7 +1,14 @@
 package com.flaringapp.ligretto.common
 
+import androidx.compose.ui.uikit.OnFocusBehavior
 import androidx.compose.ui.window.ComposeUIViewController
 import com.flaringapp.ligretto.common.ui.AppContent
 
 @Suppress("FunctionName", "unused", "ktlint:standard:function-naming")
-fun MainViewController() = ComposeUIViewController { AppContent() }
+fun MainViewController() = ComposeUIViewController(
+    configure = {
+        onFocusBehavior = OnFocusBehavior.DoNothing
+    },
+) {
+    AppContent()
+}
