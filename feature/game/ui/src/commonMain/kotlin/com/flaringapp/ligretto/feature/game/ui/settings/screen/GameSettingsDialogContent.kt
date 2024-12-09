d6e166c582fa225644112b38a040fbeafb275ea9
Andrii Shpek <ashpek01@gmail.com>
2024-12-09 16:56:54 +0200

    Implement game settings dialog content ui

--- feature/game/ui/src/commonMain/kotlin/com/flaringapp/ligretto/feature/game/ui/settings/screen/GameSettingsDialogContent.kt
+++ feature/game/ui/src/commonMain/kotlin/com/flaringapp/ligretto/feature/game/ui/settings/screen/GameSettingsDialogContent.kt
@@ -1,6 +1,8 @@
 package com.flaringapp.ligretto.feature.game.ui.settings.screen
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.SettingsInputComposite
 import androidx.compose.material3.AlertDialog
@@ -9,11 +11,25 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.flaringapp.ligretto.feature.game.ui.common.endconditions.GameEndConditionScoreIntent
+import com.flaringapp.ligretto.feature.game.ui.common.endconditions.GameEndConditionScoreLimitState
+import com.flaringapp.ligretto.feature.game.ui.common.endconditions.GameEndConditionTimeIntent
+import com.flaringapp.ligretto.feature.game.ui.common.endconditions.GameEndConditionTimeLimitState
+import com.flaringapp.ligretto.feature.game.ui.common.endconditions.ui.options.ScoreOptions
+import com.flaringapp.ligretto.feature.game.ui.common.endconditions.ui.options.TimeOptions
 import com.flaringapp.ligretto.feature.game.ui.settings.GameSettingsIntent
 import com.flaringapp.ligretto.feature.game.ui.settings.GameSettingsState
+import com.flaringapp.ligretto.feature.game.ui.settings.screen.endconditions.ConditionSettings
+import com.flaringapp.ligretto.feature.game.ui.settings.screen.endconditions.GameSettingsEndConditionsScope
+import com.flaringapp.ligretto.feature.game.ui.settings.screen.endconditions.LabeledOptions
 import ligretto_companion.core.ui.generated.resources.cancel
 import ligretto_companion.feature.game.ui.generated.resources.Res
 import ligretto_companion.feature.game.ui.generated.resources.game_settings_save_button
+import ligretto_companion.feature.game.ui.generated.resources.game_settings_score_end_condition_label
+import ligretto_companion.feature.game.ui.generated.resources.game_settings_score_end_condition_name
+import ligretto_companion.feature.game.ui.generated.resources.game_settings_time_end_condition_label
+import ligretto_companion.feature.game.ui.generated.resources.game_settings_time_end_condition_name
 import ligretto_companion.feature.game.ui.generated.resources.game_settings_title
 import org.jetbrains.compose.resources.stringResource
 import ligretto_companion.core.ui.generated.resources.Res as CoreRes
@@ -38,6 +54,7 @@ internal fun GameSettingsDialogContent(
         },
         text = {
             ActualContent(
+                modifier = Modifier.padding(top = 8.dp),
                 state = state,
                 dispatch = dispatch,
             )
@@ -61,5 +78,76 @@ internal fun GameSettingsDialogContent(
 private fun ActualContent(
     state: GameSettingsState,
     dispatch: (GameSettingsIntent) -> Unit,
+    modifier: Modifier = Modifier,
 ) {
+    Column(
+        modifier = modifier,
+    ) {
+        GameSettingsEndConditionsScope.EndConditionsScoreContent(
+            state = state.score,
+            dispatch = dispatch,
+        )
+
+        GameSettingsEndConditionsScope.EndConditionsTimeContent(
+            modifier = Modifier.padding(top = 16.dp),
+            state = state.time,
+            dispatch = dispatch,
+        )
+    }
+}
+
+@Composable
+private fun GameSettingsEndConditionsScope.EndConditionsScoreContent(
+    state: GameEndConditionScoreLimitState,
+    dispatch: (GameSettingsIntent.EndConditions.Score) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    ConditionSettings(
+        modifier = modifier,
+        conditionName = stringResource(Res.string.game_settings_score_end_condition_name),
+        selected = state.isEnabled,
+        onSelectionChanged = {
+            val intent = GameEndConditionScoreIntent.SetEnabled(it)
+            dispatch(GameSettingsIntent.EndConditions.Score(intent))
+        },
+    ) {
+        LabeledOptions(
+            label = stringResource(Res.string.game_settings_score_end_condition_label),
+        ) {
+            ScoreOptions(
+                state = state,
+                dispatch = {
+                    dispatch(GameSettingsIntent.EndConditions.Score(it))
+                },
+            )
+        }
+    }
+}
+
+@Composable
+private fun GameSettingsEndConditionsScope.EndConditionsTimeContent(
+    state: GameEndConditionTimeLimitState,
+    dispatch: (GameSettingsIntent.EndConditions.Time) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    ConditionSettings(
+        modifier = modifier,
+        conditionName = stringResource(Res.string.game_settings_time_end_condition_name),
+        selected = state.isEnabled,
+        onSelectionChanged = {
+            val intent = GameEndConditionTimeIntent.SetEnabled(it)
+            dispatch(GameSettingsIntent.EndConditions.Time(intent))
+        },
+    ) {
+        LabeledOptions(
+            label = stringResource(Res.string.game_settings_time_end_condition_label),
+        ) {
+            TimeOptions(
+                state = state,
+                dispatch = {
+                    dispatch(GameSettingsIntent.EndConditions.Time(it))
+                },
+            )
+        }
+    }
 }
