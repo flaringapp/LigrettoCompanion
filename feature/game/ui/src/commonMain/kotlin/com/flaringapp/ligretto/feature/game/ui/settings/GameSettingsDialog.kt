e1f01d8909393f02cdbf898266e556b6e1003590
Andrii Shpek <ashpek01@gmail.com>
2024-12-14 10:22:23 +0200

    Add missing settings dialog content rendering

--- feature/game/ui/src/commonMain/kotlin/com/flaringapp/ligretto/feature/game/ui/settings/GameSettingsDialog.kt
+++ feature/game/ui/src/commonMain/kotlin/com/flaringapp/ligretto/feature/game/ui/settings/GameSettingsDialog.kt
@@ -1,7 +1,10 @@
 package com.flaringapp.ligretto.feature.game.ui.settings
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.flaringapp.ligretto.core.arch.ConsumeEffects
+import com.flaringapp.ligretto.feature.game.ui.settings.screen.GameSettingsDialogContent
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
@@ -9,9 +12,17 @@ internal fun GameSettingsDialog(
     close: () -> Unit,
     store: GameSettingsViewModel = koinViewModel(),
 ) {
+    val state by store.observeState().collectAsStateWithLifecycle()
+
     ConsumeEffects(store.observeEffect()) { effect ->
         when (effect) {
             GameSettingsEffect.Close -> close()
         }
     }
+
+    GameSettingsDialogContent(
+        state = state,
+        dispatch = store::dispatch,
+        close = close,
+    )
 }
