058d2132fd4e1ce4e61c7eade4a4899ad89e8e22
Andrii Shpek <ashpek01@gmail.com>
2024-12-14 14:27:00 +0200

    Update game start previews

--- feature/game/ui/src/androidMain/kotlin/com/flaringapp/ligretto/feature/game/ui/start/screen/GameStartStateProvider.kt
+++ feature/game/ui/src/androidMain/kotlin/com/flaringapp/ligretto/feature/game/ui/start/screen/GameStartStateProvider.kt
@@ -5,18 +5,68 @@ import com.flaringapp.ligretto.feature.game.ui.start.GameStartState
 
 internal class GameStartStateProvider : PreviewParameterProvider<GameStartState> {
 
-    override val values: Sequence<GameStartState> = sequenceOf(
-        GameStartState(
+    companion object {
+
+        fun initial() = GameStartState()
+
+        fun endConditionsExpandedConditionsSomethingSelected() = with(initial()) {
+            copy(
+                endConditions = endConditions.copy(
+                    score = endConditions.score.copy(isEnabled = true),
+                ),
+            )
+        }
+
+        fun endConditionsExpandedOptionsSomethingSelected() = with(
+            endConditionsExpandedConditionsSomethingSelected(),
+        ) {
+            copy(
+                endConditions = endConditions.copy(
+                    isExpandedConditionsCompleted = true,
+                ),
+            )
+        }
+
+        fun endConditionsExpandedOptionsEmpty() = GameStartState(
+            endConditions = GameStartState.EndConditions(
+                isExpandedConditionsCompleted = true,
+            ),
+        )
+
+        fun playersEmpty() = GameStartState(
             players = GameStartPlayersProvider.empty(),
-            endConditions = GameStartState.EndConditions(),
-        ),
-        GameStartState(
-            players = GameStartPlayersProvider.short(),
-            endConditions = GameStartState.EndConditions(),
-        ),
-        GameStartState(
-            players = GameStartPlayersProvider.long(),
-            endConditions = GameStartState.EndConditions(),
-        ),
+            endConditions = GameStartState.EndConditions(
+                isExpandedConditionsCompleted = true,
+                isExpandedOptionsCompleted = true,
+            ),
+        )
+
+        fun playersShort() = with(playersEmpty()) {
+            copy(
+                players = GameStartPlayersProvider.short(),
+                endConditions = endConditions.copy(
+                    score = endConditions.score.copy(isEnabled = true),
+                ),
+            )
+        }
+
+        fun playersLong() = with(playersShort()) {
+            copy(
+                players = GameStartPlayersProvider.long(),
+                endConditions = endConditions.copy(
+                    time = endConditions.time.copy(isEnabled = true),
+                ),
+            )
+        }
+    }
+
+    override val values: Sequence<GameStartState> = sequenceOf(
+        initial(),
+        endConditionsExpandedConditionsSomethingSelected(),
+        endConditionsExpandedOptionsSomethingSelected(),
+        endConditionsExpandedOptionsEmpty(),
+        playersEmpty(),
+        playersShort(),
+        playersLong(),
     )
 }
