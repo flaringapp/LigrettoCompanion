8f0e26085bfa2f4fe60d09ca1765d4981f4dcfca
Andrii Shpek <ashpek01@gmail.com>
2025-05-08 12:37:00 +0300

    Format code

--- core/di/src/commonMain/kotlin/com/flaringapp/ligretto/core/di/DiDefinitionScope.kt
+++ core/di/src/commonMain/kotlin/com/flaringapp/ligretto/core/di/DiDefinitionScope.kt
@@ -4,5 +4,5 @@ package com.flaringapp.ligretto.core.di
  * A scope, receiver for DI module provider methods
  */
 interface DiDefinitionScope {
-    companion object : DiDefinitionScope;
+    companion object : DiDefinitionScope
 }
