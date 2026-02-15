89bffcef2682a583f5fe21a58f3d213a9ef292e1
Andrii Shpek <andrii.shpek@prjheadway.com>
2026-02-15 16:43:10 +0200

    Migrate multiplatform preview annotation to androidx

--- core/ui/src/commonMain/kotlin/com/flaringapp/ligretto/core/ui/components/FooterButton.kt
+++ core/ui/src/commonMain/kotlin/com/flaringapp/ligretto/core/ui/components/FooterButton.kt
@@ -13,12 +13,12 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
 import com.flaringapp.ligretto.core.designsystem.AppTheme
 import com.flaringapp.ligretto.core.ui.ext.screen
-import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun FooterButton(
